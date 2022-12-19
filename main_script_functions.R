96854c9760bdc219adb371cf582cd962422a4e8f
Paul Carvalho <pcarvalh@ucsc.edu>
2022-12-19 15:39:46 -0800

    edited script header

--- main_script_functions.R
+++ main_script_functions.R
@@ -1,11 +1,8 @@
-#### 
-## main_script_functions.R
-##
+## -------------------------------------------------------------------------------------------------------------------------
 ## Author: Paul Carvalho (paul.carvalho@noaa.gov, pcarvalh@ucsc.edu)
-## Last update: 10/11/2022
 ##
-## Description: Functions used in the in the main script of the simulation models
-####
+## Description: Supporting functions used in 'main_script.R'
+## -------------------------------------------------------------------------------------------------------------------------
 
 calc_overfished <- function(mod, n.sim, n.yr){
   # Set up parallel backend to run multiple simulations simultaneously
